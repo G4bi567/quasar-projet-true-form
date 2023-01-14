4ed0278792fc4ddcb15efbb512f23a35e43b1a45
G4bi567 <107567915+G4bi567@users.noreply.github.com>
2023-01-14 09:34:11 +0100

    3 files modified

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -189,7 +189,7 @@ const ThepostList = reactive([
     date: '22-11-22',
     description:
       "Bonjour, ça fait longtemps j'ai un problème avec les résolutions de problèmes en Maths, car je ne vois pas où commencer, quoi utiliser... etc. Merci de votre aide",
-    branche: 'fr',
+    branche: 'Mathématiques',
     pp_profil:
       'https://th.bing.com/th/id/R.5a537e0b861eb2177ae056f26c87c097?rik=OaqK6FPDWti%2f4g&riu=http%3a%2f%2fwww.magicalmaths.org%2fwp-content%2fuploads%2f2012%2f11%2fmaths_image.jpg&ehk=Ef%2fTZg08YV4z5c1PxaQy1rYcsmjGbiEAxH3L6ifAk3o%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
     id: 2,
