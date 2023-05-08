f031beb8927ba96c25c2e11c3ff1c8e657de649d
G4bi567 <107567915+G4bi567@users.noreply.github.com>
2023-05-08 17:11:23 +0200

    5 files modified, 1 added, 1 deleted

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,20 @@
 </template>
 
 <script setup>
-import { useClient } from 'villus';
+import { useClient, defaultPlugins } from 'villus';
 import { defineComponent } from 'vue';
 
 defineComponent({
   name: 'App',
 });
+function authPlugin({ opContext }) {
+  opContext.headers['Content-Type'] = 'application/json';
+  opContext.headers['x-hasura-admin-secret'] =
+    'Il0IFkTm1C3SgZPk1y1hrp4hsidxML2uNyswlzrMH3l0kRQLxQnWNfFIzE1IJ9cy';
+}
 
 useClient({
   url: 'https://pleased-spaniel-49.hasura.app/v1/graphql',
-  headers: {
-    'Content-Type': 'application/json',
-    'x-hasura-admin-secret':
-      'Il0IFkTm1C3SgZPk1y1hrp4hsidxML2uNyswlzrMH3l0kRQLxQnWNfFIzE1IJ9cy',
-  },
+  use: [authPlugin, ...defaultPlugins()],
 });
 </script>
---
+++ src/pages/TableUser.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    Bonjour
+    <ul v-if="data">
+      <li v-for="user in data.users" :key="user.id">
+        {{ user.username }} | {{ user.email }} | {{ user.password_hash }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { useQuery } from 'villus';
+import { gql } from 'graphql-tag';
+
+const GetUsers = gql`
+  query GetAllUsers {
+    user {
+      id
+      username
+      email
+      password_hash
+    }
+  }
+
+`;
+
+const { data } = useQuery({
+  query: GetUsers,
+});
+</script>
