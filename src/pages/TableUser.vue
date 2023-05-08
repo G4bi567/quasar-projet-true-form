9cd1c595c973c24e5559c4f2c96006862499a3fa
G4bi567 <107567915+G4bi567@users.noreply.github.com>
2023-05-08 19:13:12 +0200

    addprofil

--- src/pages/TableUser.vue
+++ src/pages/TableUser.vue
@@ -1,31 +1,50 @@
 <template>
   <div>
-    Bonjour
-    <ul v-if="data">
-      <li v-for="user in data.users" :key="user.id">
-        {{ user.username }} | {{ user.email }} | {{ user.password_hash }}
-      </li>
-    </ul>
+    <div v-if="data">
+      <p>User created: {{ data.createUser.username }}</p>
+    </div>
+    <button @click="onSubmit('jojaja', 'gagag@fdhsfsdahj.com', 'here')">
+      Submit
+    </button>
   </div>
 </template>
 
 <script setup>
-import { useQuery } from 'villus';
-import { gql } from 'graphql-tag';
+import { useQuery, useMutation } from 'villus';
+import { ref } from 'vue';
 
-const GetUsers = gql`
-  query GetAllUsers {
-    user {
+const CreateUserAndInsertReview = `
+  mutation CreateUserAndInsertReview(
+    $username: String!,
+    $email: String!,
+    $password_hash: String!,
+  ) {
+    insert_user_one(object: { username: $username, email: $email, password_hash: $password_hash}) {
       id
       username
       email
       password_hash
+      profil_photo
     }
   }
-
 `;
+const { data, execute } = useMutation(CreateUserAndInsertReview);
+const variables = {
+  username: '',
+  email: '',
+  password_hash: '',
+};
 
-const { data } = useQuery({
-  query: GetUsers,
-});
+function onSubmit(username, email, password_hash) {
+  variables.username = username;
+  variables.email = email;
+  variables.password_hash = password_hash;
+  alert(1);
+  execute(variables).then((result) => {
+    if (result.error) {
+      alert(2);
+      alert(`${result.error}`);
+    }
+  });
+}
 </script>
