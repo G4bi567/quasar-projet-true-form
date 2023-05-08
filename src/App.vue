3b8e1d41180929f63b094784a06f628ee4e425ae
G4bi567 <107567915+G4bi567@users.noreply.github.com>
2023-05-08 17:00:03 +0200

    4 files modified, 2 added

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,20 @@
   <router-view />
 </template>
 
-<script>
+<script setup>
+import { useClient } from 'villus';
 import { defineComponent } from 'vue';
 
-export default defineComponent({
+defineComponent({
   name: 'App',
+});
+
+useClient({
+  url: 'https://pleased-spaniel-49.hasura.app/v1/graphql',
+  headers: {
+    'Content-Type': 'application/json',
+    'x-hasura-admin-secret':
+      'Il0IFkTm1C3SgZPk1y1hrp4hsidxML2uNyswlzrMH3l0kRQLxQnWNfFIzE1IJ9cy',
+  },
 });
 </script>
