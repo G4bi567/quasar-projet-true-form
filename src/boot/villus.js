2ecd53f1bd083dbf629bdc92cde7686f725ed27f
G4bi567 <107567915+G4bi567@users.noreply.github.com>
2023-05-15 13:33:52 +0200

    1 file modified

--- src/boot/villus.js
+++ src/boot/villus.js
@@ -1,18 +1,15 @@
-// src/boot/villus.js
-import { useClient, defaultPlugins } from 'villus';
+import { createClient, defaultPlugins } from '@villus/core';
 import { boot } from 'quasar/wrappers';
 
 export default boot(({ app }) => {
-  function authPlugin({ opContext }) {
-    opContext.headers['Content-Type'] = 'application/json';
-    opContext.headers['x-hasura-admin-secret'] =
-      'Il0IFkTm1C3SgZPk1y1hrp4hsidxML2uNyswlzrMH3l0kRQLxQnWNfFIzE1IJ9cy';
-  }
+  const client = createClient({
+    url: 'https://pleased-spaniel-49.hasura.app/v1/graphql',
+    use: [...defaultPlugins()],
+    headers: {
+      'Content-Type': 'application/json',
+      'x-hasura-admin-secret': 'Il0IFkTm1C3SgZPk1y1hrp4hsidxML2uNyswlzrMH3l0kRQLxQnWNfFIzE1IJ9cy',
+    },
+  });
 
-  app.provide(
-    useClient({
-      url: 'https://pleased-spaniel-49.hasura.app/v1/graphql',
-      use: [authPlugin, ...defaultPlugins()],
-    })
-  );
+  app.provide(client);
 });
