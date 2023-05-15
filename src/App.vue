3942234ad1a0051a72a11e9f9693f4ff1e2c2634
G4bi567 <107567915+G4bi567@users.noreply.github.com>
2023-05-15 13:30:24 +0200

    3 files modified

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,9 @@
 </template>
 
 <script setup>
-import { useClient, defaultPlugins } from 'villus';
 import { defineComponent } from 'vue';
 
 defineComponent({
   name: 'App',
-});
-function authPlugin({ opContext }) {
-  opContext.headers['Content-Type'] = 'application/json';
-  opContext.headers['x-hasura-admin-secret'] =
-    'Il0IFkTm1C3SgZPk1y1hrp4hsidxML2uNyswlzrMH3l0kRQLxQnWNfFIzE1IJ9cy';
-}
-
-useClient({
-  url: 'https://pleased-spaniel-49.hasura.app/v1/graphql',
-  use: [authPlugin, ...defaultPlugins()],
 });
 </script>
--- src/boot/villus.js
+++ src/boot/villus.js
@@ -1,7 +1,18 @@
-import { useClient } from '@villus/core';
+// src/boot/villus.js
+import { useClient, defaultPlugins } from 'villus';
+import { boot } from 'quasar/wrappers';
 
-export default async ({ app }) => {
-  app.provide(useClient({
-    url: 'http://localhost:4000/graphql', // replace with your GraphQL endpoint
-  }));
-};
+export default boot(({ app }) => {
+  function authPlugin({ opContext }) {
+    opContext.headers['Content-Type'] = 'application/json';
+    opContext.headers['x-hasura-admin-secret'] =
+      'Il0IFkTm1C3SgZPk1y1hrp4hsidxML2uNyswlzrMH3l0kRQLxQnWNfFIzE1IJ9cy';
+  }
+
+  app.provide(
+    useClient({
+      url: 'https://pleased-spaniel-49.hasura.app/v1/graphql',
+      use: [authPlugin, ...defaultPlugins()],
+    })
+  );
+});
